df082180585344289e9932b5fd5836c99048021f
daria marczak <marczak.ria@gmail.com>
2017-11-01 16:32:56 +0100

    changed variables

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-function getTriangleArea(a,h) {
-    if (a < 0) {
+function getTriangleArea(a, h) {
+    if (a < 0 || h < 0){
         return "Invalid data";
     }
-    if (h < 0) {
-        return "Invalid data";
-    }
+
     else {
-    return a * h / 2;
+        return a * h / 2;
     }
 }
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea(10, 15);
+var triangle1Area = getTriangleArea(10, -15);
 var triangle2Area = getTriangleArea(30, 40);
-var triangle3Area = getTriangleArea(3, 5);
+var triangle3Area = getTriangleArea(-3, 5);
+
+console.log(getTriangleArea(-3,5));
