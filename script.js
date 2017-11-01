af0950850027344e9da9d6bce5ae28ec8812e4c9
daria marczak <marczak.ria@gmail.com>
2017-11-01 17:59:20 +0100

    modified

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
 function getTriangleArea(a, h) {
-    if (a < 0 || h < 0){
+    if (a < 0 || h < 0) {
         return "Invalid data";
-    }
-
-    else {
+    } else {
         return a * h / 2;
     }
 }
@@ -14,4 +12,4 @@ var triangle1Area = getTriangleArea(10, -15);
 var triangle2Area = getTriangleArea(30, 40);
 var triangle3Area = getTriangleArea(-3, 5);
 
-console.log(getTriangleArea(-3,5));
+console.log(getTriangleArea(-3, 5));
